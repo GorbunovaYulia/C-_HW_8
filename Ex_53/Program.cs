b32f334b4aeacb8d400e98ed5ca957e51ec906a5
yulia <yuliagorbunova@yahoo.com>
2023-02-12 12:55:36 +0500

    start

---
+++ Ex_53/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+53. Задайте двумерный массив.
+Поменять местами первую и последнюю строчку массива.
+*/
+
+int[,] generate2DArray(int colLength, int rowLength, int start, int finish)
+{
+    int[,] array= new int[colLength, rowLength];
+    for (int i = 0; i < colLength; i++)
+    {
+        for (int j = 0; j < rowLength; j++)
+        {
+            array[i,j] = new Random().Next(start, finish+1);
+        }
+        
+    }
+    return array;
+}
+
+void printInColor (string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write("\t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void printArray(int[,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = generate2DArray(10,7,-5,10);
+printArray(array);
