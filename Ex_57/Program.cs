048c732f2d27e361fb3473de28551eac070298a2
yulia <yuliagorbunova@yahoo.com>
2023-02-12 15:18:51 +0500

    Ex_57

---
+++ Ex_57/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 57. Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, сколько рз встречается элемент входных данных.
+Частотный массив
+1, 9, 9, 8, 2, 8, 0, 9
+0 встречается 1 раз
+9 встречаетяс 3 раза
+и тд.
+*/
+
+int[,] generate2DArray(int rowLength, int colLength, int start, int finish)
+{
+    int[,] array = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+
+    }
+    return array;
+}
+
+void printInColor(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write("\t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void printArray(int[,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("-----------------------------------------------------");
+}
+
+int[] getFrequencyDictionary(int[,] array, int maxNumber)
+{
+    int[] result = new int[maxNumber];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[array[i, j]] += 1;
+        }
+    }
+    return result;
+}
+
+void printFrequencyDictionary(int[] FrequencyDictionary)
+{
+    for (int i = 0; i < FrequencyDictionary.Length; i++)
+    {
+        if (FrequencyDictionary[i] > 0)
+        {Console.WriteLine($"{i} встречается {FrequencyDictionary[i]} раза");}
+    }
+}
+
+int[,] array = generate2DArray(5, 5, 0, 20);
+printArray(array);
+int[] FrequencyDictionary = getFrequencyDictionary(array, 21);
+printFrequencyDictionary(FrequencyDictionary);
---
+++ HW_54/Program.cs
@@ -0,0 +1,87 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+
+int[,] generate2DArray(int rowLength, int colLength, int start, int finish)
+{
+    int[,] array = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+
+    }
+    return array;
+}
+
+void printInColor(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write("\t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void printArray(int[,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("-----------------------------------------------------");
+}
+
+int getDataFromUser(string message)
+{
+    printInColor(message + "\n", ConsoleColor.Yellow);
+    int data = int.Parse(Console.ReadLine()!);
+    return data;
+}
+
+int[,] rotateMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+        }
+
+    }
+    return array;
+}
+
+int rows = getDataFromUser("Введите количество строк");
+int cols = getDataFromUser("Введите количество столбцов");
+int[,] array = generate2DArray(rows, cols, 0, 50);
+printArray(array);
+int[,] rotateArray = rotateMatrix(array);
+printArray(rotateArray);
