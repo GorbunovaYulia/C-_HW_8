8c6a798d3459707003eacb2dc5f47f79029e6d9e
yulia <yuliagorbunova@yahoo.com>
2023-02-15 18:22:09 +0500

    HW_54

--- HW_54/Program.cs
+++ HW_54/Program.cs
@@ -64,15 +64,23 @@ int getDataFromUser(string message)
     return data;
 }
 
-int[,] rotateMatrix(int[,] array)
+int[,] selectionSort(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1) - 1; j++)
         {
+            int maxPosition = j;
+            for (int k = j + 1; k < array.GetLength(1); k++)
+            {
+                if (array[i, k] > array[i, maxPosition])
+                {
+                    maxPosition = k;
+                }
+            }
             int temp = array[i, j];
-            array[i, j] = array[j, i];
-            array[j, i] = temp;
+            array[i, j] = array[i, maxPosition];
+            array[i, maxPosition] = temp;
         }
 
     }
@@ -83,5 +91,5 @@ int rows = getDataFromUser("Введите количество строк");
 int cols = getDataFromUser("Введите количество столбцов");
 int[,] array = generate2DArray(rows, cols, 0, 50);
 printArray(array);
-int[,] rotateArray = rotateMatrix(array);
-printArray(rotateArray);
+int[,] sortArray = selectionSort(array);
+printArray(sortArray);
