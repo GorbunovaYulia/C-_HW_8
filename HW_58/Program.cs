3f473378d080a69c99f13f31d20171c316efe359
yulia <yuliagorbunova@yahoo.com>
2023-02-16 21:06:49 +0500

    HW_58

---
+++ HW_58/Program.cs
@@ -0,0 +1,90 @@
+﻿/*
+Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+int[,] generate2DArray(int rowLength, int colLength, int start, int finish)
+{
+    int[,] array = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            array[i, j] = new Random().Next(start, finish + 1);
+        }
+
+    }
+    return array;
+}
+
+void printInColor(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write("\t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void printArray(int[,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine("-----------------------------------------------------");
+}
+
+int getDataFromUser(string message)
+{
+    printInColor(message + "\n", ConsoleColor.Yellow);
+    int data = int.Parse(Console.ReadLine()!);
+    return data;
+}
+
+int[,] findMultiplyArrays(int[,] array1, int[,] array2)
+{
+    int rowLength = array1.GetLength(0);
+    int colLength = array2.GetLength(1);
+    int[,] arrayMyltiply = new int[rowLength, colLength];
+    for (int i = 0; i < rowLength; i++)
+    {
+        for (int j = 0; j < colLength; j++)
+        {
+            arrayMyltiply[i, j] = array1[i, 0] * array2[0, j];
+            for (int k = 1; k < colLength; k++)
+            {
+                arrayMyltiply[i, j] = arrayMyltiply[i, j] + array1[i, k] * array2[k, j];
+            }
+        }
+    }
+    return arrayMyltiply;
+}
+
+int start = getDataFromUser("Введите начальное значение диапазона чисел");
+int finish = getDataFromUser("Введите конечное значение диапазона чисел");
+int[,] array1 = generate2DArray(5, 4, start, finish);
+int[,] array2 = generate2DArray(4, 3, start, finish);
+printArray(array1);
+printArray(array2);
+int[,] MultiplyArrays = findMultiplyArrays(array1, array2);
+printArray(MultiplyArrays);
---
+++ HW_62/Program.cs
@@ -0,0 +1,102 @@
+﻿/*
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+/*int[] array = new int[n - m + 1];
+        for (int i = 0; i < array.Length; i++) {
+            array[i] = m++;
+        }
+*/
+
+int[,] generate2DArray(int length)
+{
+    int[,] array = new int[4, 4];
+    int n = 1;
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            array[i, j] = n++;
+        }
+
+    }
+    return array;
+}
+
+void printNewArray(int[,] array)
+{
+    int i = 0;
+    int j = 0;
+    for (j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    for (i = 1; i < array.GetLength(0); i++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    for (j = array.GetLength(1) - 1; j > -1; j--)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+        for (i = 1; i < array.GetLength(0); i++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+}
+
+void printInColor(string data, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(data);
+    Console.ResetColor();
+}
+
+void printHeadOfArray(int length)
+{
+    Console.Write("\t");
+    for (int i = 0; i < length; i++)
+    {
+        printInColor(i + "\t", ConsoleColor.DarkGreen);
+    }
+    Console.WriteLine();
+}
+
+void printArray(int[,] array)
+{
+    printHeadOfArray(array.GetLength(1));
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printInColor(i + "\t", ConsoleColor.Cyan);
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int getDataFromUser(string message)
+{
+    printInColor(message + "\n", ConsoleColor.Yellow);
+    int data = int.Parse(Console.ReadLine()!);
+    return data;
+}
+
+
+int[,] array = generate2DArray(4);
+printArray(array);
+Console.WriteLine();
+Console.WriteLine();
+printNewArray(array);
+
+
